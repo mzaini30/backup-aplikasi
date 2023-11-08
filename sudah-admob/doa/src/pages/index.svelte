8e5678edcd670ac8563cad1de1dcad8fa88f4a6a
mzaini30 <muhzaini30@gmail.com>
2023-11-09 07:18:54 +0800

    amankan

---
+++ sudah-admob/doa/src/pages/index.svelte
@@ -0,0 +1,12 @@
+<script>
+  import data from "../data.json";
+  import { push } from "svelte-spa-router";
+</script>
+
+<div class="p-4">
+  <ul class="menu bg-base-200 w-full rounded-box">
+    {#each data as x}
+      <li><a href="#/doa/{x.id}">{x.judul}</a></li>
+    {/each}
+  </ul>
+</div>
